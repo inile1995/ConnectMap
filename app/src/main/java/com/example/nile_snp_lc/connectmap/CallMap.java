c58b71a3eb22195066e312d4831a4a3b15cb0b7c
NILE_SNP-LC <inile1995@gmail.com>
2017-04-20 11:47:52 +0700

    Test Map

--- app/src/main/java/com/example/nile_snp_lc/connectmap/CallMap.java
+++ app/src/main/java/com/example/nile_snp_lc/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package com.example.nile_snp_lc.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -10,4 +14,26 @@ public class CallMap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_map);
     } // End onCreate main method
+
+    public void runCallMap(View view){
+        // Initail View ผูกตัวแปรบน Java ให่้รู้จักกับ xml
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlabel = label.getText().toString().trim();
+
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.759617,100.542439(ศูนย์รางน้ำ)");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW , location);
+        //mapIntent.setPackage("com.google.android.apps.maps"); // set ให้ใช้ GoogleMap โดยตรงใช้ได้กับโทรศัพท์android และ ใช้ไม่ได้กับ Genemotion
+        startActivity(mapIntent);
+
+    } // End void RunCallMap
+
+
+
+
 } //End main Class
